7c635a3c5e5c686a6075a6675e185d0326144086
Tarsoly András <tarsolya@gmail.com>
2011-07-31 12:41:57 +0200

    Add migration number for migrations.

--- lib/generators/commentary_generator.rb
+++ lib/generators/commentary_generator.rb
@@ -9,4 +9,12 @@ class CommentaryGenerator < Rails::Generators::NamedBase
     migration_template 'create_commentary.rb', File.join("db/migrate", "create_commentary.rb")
   end
 
+  def self.next_migration_number(dirname)
+    if ActiveRecord::Base.timestamped_migrations
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
+    else
+      "%.3d" % (current_migration_number(dirname) + 1)
+    end
+  end
+
 end
