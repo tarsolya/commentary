fddd781ded0bf1db7b3b59a4ee29113301e12065
Tarsoly András <tarsolya@gmail.com>
2011-09-01 22:34:57 +0200

    Nerf named_scopes.

--- lib/rating_methods.rb
+++ lib/rating_methods.rb
@@ -4,9 +4,9 @@ module Commentary
 
     def self.included(comment_model)
       comment_model.extend Finders
-      comment_model.named_scope :in_order, {:order => 'created_at ASC'}
-      comment_model.named_scope :recent, {:order => "created_at DESC"}
-      comment_model.named_scope :limit, lambda {|limit| {:limit => limit}}
+      comment_model.scope :in_order, {:order => 'created_at ASC'}
+      comment_model.scope :recent, {:order => "created_at DESC"}
+      comment_model.scope :limit, lambda {|limit| {:limit => limit}}
     end
 
     module Finders
